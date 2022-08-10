1761fad5896efb90b0af17645de51325344fd483
Artur <magisterbrownie@gmail.com>
2022-08-10 23:51:01 +0200

    Train loop half percision

--- feedback/training/loop.py
+++ feedback/training/loop.py
@@ -6,30 +6,34 @@ import gc
 def train_epoch(dataloader: DataLoader, model: nn.Module, optimizer, scheduler=None, device=torch.device("cpu")):
     model.train()
     loss = nn.CrossEntropyLoss()
-    
+    scaler = torch.cuda.amp.GradScaler()
+
     dataset_size = 0
-    running_loss = 0.0
-    
+    running_loss = torch.tensor(0.0,device=device)
     for step, data in enumerate(dataloader):
-        
+        optimizer.zero_grad()
+
         ids = data['input_ids'].to(device, dtype = torch.long)
         mask = data['attention_mask'].to(device, dtype = torch.long)
         targets = data['target'].to(device, dtype = torch.long)
-        
+
         batch_size = ids.size(0)
-        
-        outputs = model(ids, mask)
-        lss = loss(outputs.logits, targets)
-        lss.backward()
-        optimizer.step()
-        
-        running_loss += (lss.item() * batch_size)
-        dataset_size += batch_size
-        
+        with torch.cuda.amp.autocast():
+          outputs = model(ids, mask)
+          lss = loss(outputs.logits, targets)
+
+        scaler.scale(lss).backward()
+        scaler.step(optimizer)
+
+        scaler.update()
+
+        running_loss += lss
+        dataset_size += 1
+
         if scheduler:
             scheduler.step()
-    
-    final_loss = running_loss / dataset_size
+
+    final_loss = running_loss.item() / dataset_size
     gc.collect()
     return final_loss
     
